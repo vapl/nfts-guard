07a23954586ee1a7530c31ece575b4b10649509d
vapl <valdis.vascenkovs@gmail.com>
2025-03-20 13:06:18 +0200

    Added .eslintrc.json, Modified next.config and tsconfig files to fix eslint error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     domains: ["boredapeyachtclub.com", "larvalabs.com", "opensea.io"], // Pievieno visus NFT attēlu hostus
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true, // 301 Redirect (SEO draudzīgi)
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
