547d4ff0f3b30507a5d283e7cb156a43abcb3683
vapl <valdis.vascenkovs@gmail.com>
2025-03-22 13:14:55 +0200

    Fixed email

--- src/utils/emailTemplate.ts
+++ src/utils/emailTemplate.ts
@@ -469,18 +469,11 @@ export const generateSubscribeEmailContent = () => {
               target="_blank"
               style="display: inline-block"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="black"
-                class="bi bi-twitter-x"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"
-                />
-              </svg>
+              <img 
+                src="https://nftsguard.com/image/icons/x-social-icon.svg"
+                alt="Twitter"
+                style="width: 28px; height: 28px"
+              />
             </a>
             <a
               href="https://discord.gg/zqhEbgEsur"
