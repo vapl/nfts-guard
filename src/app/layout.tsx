ce9418d3b66777dea7918cb3a6aa9672f23dff5b
vapl <valdis.vascenkovs@gmail.com>
2025-03-23 11:26:46 +0200

    Added Google analitics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ThemeProvider from "@/components/ClientProvider";
 import "@/app/globals.css";
 
 import { LayoutProps } from "@/types/layout";
+import Script from "next/script";
 
 // Fontu inicializācija
 const geistSans = Geist({
@@ -33,6 +34,26 @@ export default async function RootLayout({ children, params }: LayoutProps) {
       lang={locale}
       className={`${geistSans.variable} ${geistMono.variable} ${michroma.variable}`}
     >
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-DMM184GK4N"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DMM184GK4N');
+            `,
+          }}
+        />
+      </head>
+
       <body className="antialiased bg-background text-text transition-colors duration-300">
         <ThemeProvider>
           <ScanProvider>{children}</ScanProvider>
