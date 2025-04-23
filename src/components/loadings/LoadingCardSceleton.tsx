93b61219f40c48f78b8e2ac5eeb2028695eef5cb
Valdis Vascenkovs <45950903+vapl@users.noreply.github.com>
2025-04-23 12:05:09 +0300

    Update LoadingCardSceleton.tsx x padding

--- src/components/loadings/LoadingCardSceleton.tsx
+++ src/components/loadings/LoadingCardSceleton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingCardSkeleton: React.FC = () => {
   return (
-    <div className="w-full py-8 px-8 lg:px-16 xl:px-24">
+    <div className="w-full py-8 px-4 lg:px-16 xl:px-24">
       <div className="w-full bg-gradient-to-r bg-card rounded-2xl p-6 flex flex-col sm:flex-row items-center justify-between gap-6 shadow-lg animate-pulse">
         <div className="flex w-full sm:w-auto items-center justify-between">
           <div className="relative w-32 h-32 rounded-xl bg-gray-300"></div>
